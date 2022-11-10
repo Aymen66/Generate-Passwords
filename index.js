a1a38ffaf925bfce53974da27ac64d7bb7df5e86
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-10 01:29:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ generateButton.addEventListener("click", function () {
 
    //  
 
+let copyNote = document.getElementById("copyNote")
 
+copyNote.innerHTML = "<p>Press the password to copy to the clipboard </p>"
 })
 function copy(that){
     var inp =document.createElement('input');
@@ -60,42 +62,3 @@ function copy(that){
 
 
 
-
-/*
-
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-let generatePasswordsOne =document.getElementById("generate-passwords-one")
-let generatePasswordsTwo =document.getElementById("generate-passwords-two")
-let generateButton =document.getElementById("generateButton")
-
-function generateButton(){
-    let randomIndexOne = Math.floor(Math.random() * 3)
-    let randomIndexTwo = Math.floor(Math.random() * 3)
-    generatePasswordsOne.textContent = characters[randomIndexOne]
-    generatePasswordsTwo.textContent = characters[randomIndexTwo]
-
-}generateButton()
-*/
-
-
-
-/*
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-let generatePasswordsOne =document.getElementById("generate-passwords-one")
-let generatePasswordsTwo =document.getElementById("generate-passwords-two")
-let generateButton =document.getElementById("generateButton")
-
-generateButton.addEventListener("click", function(){
-    let randomIndexOne = Math.floor(Math.random() * 3)
-    let randomIndexTwo = Math.floor(Math.random() * 3)
-    console.log(randomIndexOne)
-    generatePasswordsOne.textContent = characters[randomIndexOne]
-    generatePasswordsTwo.textContent = characters[randomIndexTwo]
-
-}
-
-)
-
-*/
